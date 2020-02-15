9233676448f3bdb0ddcc177240cfbbf513e6153e
Nicolas Raigosa <37982751+nraigosam@users.noreply.github.com>
2020-02-15 13:56:09 -0500

    sin abances

--- Reto 3/Edge.java
+++ Reto 3/Edge.java
@@ -1,10 +1,13 @@
-/**
- * Edge
- */
+import java.io.*;
 public class Edge {
+    public static void leer_archivo(String archivo){
+        
+    }
 
     public int point1() {
         int valor=0;
         return valor;
+    }
+    public static void main(String[] args) {
     }
 }
