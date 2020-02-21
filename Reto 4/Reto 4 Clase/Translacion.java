22629a06046bbeede024fa11f59cc2a5ec33d34d
James Morales <jamesmoralesmoreno@gmail.com>
2020-02-21 18:41:57 -0500

    Reto casa

---
+++ Reto 4/Reto 4 Clase/Translacion.java
@@ -0,0 +1,67 @@
+/**
+ * Translacion
+ */
+public class Translacion {
+
+    Edge puntos;
+    int dx;
+    int dy;
+
+    public Translacion(int dx, int dy, Edge puntos){
+        this.puntos= puntos;
+        this.dx=dx;
+        this.dy=dy;
+    }
+
+    public void movimiento(){
+
+        for (int i = 0; i < puntos.nodos.length; i++) {
+            Point3 nuevo_valor=nueva_posicion(this.puntos.nodos[i][0], this.puntos.nodos[i][1]);
+            this.puntos.nodos[i][0]=nuevo_valor.punto[0];
+            this.puntos.nodos[i][1]=nuevo_valor.punto[1];
+        }
+
+    }
+
+    public Point3 nueva_posicion(int x, int y){
+        Matrix3x3 matriz_translacion = new Matrix3x3();
+        int[][] matriz = new int[3][3];
+        Point3 punto= new Point3(x, y, 1);
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                switch (j) {
+                    case 0:
+                        if(i==0){
+                            matriz[i][j]=1;
+                        }else{
+                            matriz[i][j]=0;
+                        }
+                        break;
+                    case 1:
+                        if(i==1){
+                            matriz[i][j]=1;
+                        }else{
+                            matriz[i][j]=0;
+                        }
+                        break;
+                    case 2:
+                        if(i==0){
+                            matriz[i][j]=dx;
+                        }else if(i==1){
+                            matriz[i][j]=dy;
+                        }
+                        else{
+                            matriz[i][j]=1;
+                        }
+                        break;
+
+                }
+            }
+        }
+
+        return matriz_translacion.times(matriz, punto);
+    }
+    
+
+}
